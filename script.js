8b73c1d62470ad61919c7382ac0a22d4b7e6dbd1
vothanhdamkg1982-lang <vothanhdamkg1982@gmail.com>
2026-08-12 10:01:13 +0700

    Sửa 41

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ async function signInWithGoogle() {
         const { data, error } = await supabaseClient.auth.signInWithOAuth({
             provider: 'google',
             options: {
-                redirectTo: window.location.origin
+                redirectTo: window.location.origin + window.location.pathname
             }
         });
         if (error) throw error;
